0a27a87807a69590ec59451e5a65628a8d2e5269
Adrian Sieber <mail@adriansieber.com>
2026-05-12 13:32:58 +0000

    Remove server functionality

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import express, { type Request, type Response } from "express"
-
-const app = express()
-
-app.get("/:id", (request: Request, response: Response) => {
-  response.send(`${request.params.id} - description`)
-})
-
-app.listen(3000)
